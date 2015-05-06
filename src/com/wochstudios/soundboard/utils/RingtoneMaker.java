38bdafe38388dd60c414e4bd8d74f1dfa0708f76
Dave McPherson <dmcp89@gmail.com>
2015-05-06 12:11:03 -0400

    Update RingtoneMaker.java
    refactored setRingtone method into 3 methods

--- src/com/wochstudios/soundboard/utils/RingtoneMaker.java
+++ src/com/wochstudios/soundboard/utils/RingtoneMaker.java
@@ -11,21 +11,28 @@ import android.provider.MediaStore;
 
 
 public class RingtoneMaker {
+	private ContentValues values = new ContentValues();
+	
 	public void setRingtone(String name, Context con){
 		File k = new File(Environment.getExternalStorageDirectory().toString()+File.separator+"download"+File.separator+name+".mp3");
-		ContentValues values = new ContentValues();
-		   values.put(MediaStore.MediaColumns.DATA, k.getAbsolutePath());
-		   values.put(MediaStore.MediaColumns.TITLE, name);
-		   values.put(MediaStore.MediaColumns.SIZE, k.length());
-		   values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/mp3");
-		   values.put(MediaStore.Audio.Media.ARTIST, "Soundboard");
-		   values.put(MediaStore.Audio.Media.IS_RINGTONE, true);
-		   values.put(MediaStore.Audio.Media.IS_NOTIFICATION, true);
-		   values.put(MediaStore.Audio.Media.IS_ALARM, true);
-		   values.put(MediaStore.Audio.Media.IS_MUSIC, false);
-
-		   //Insert it into the database
-		   Uri uri = MediaStore.Audio.Media.getContentUriForPath(k.getAbsolutePath());
-		   con.getContentResolver().insert(uri, values);
+		putValuesFromFile(k);
+		Uri uri = MediaStore.Audio.Media.getContentUriForPath(k.getAbsolutePath());
+		con.getContentResolver().insert(uri, values);
+	}
+	
+	private void putValuesFromFile(File k){
+		values.put(MediaStore.MediaColumns.DATA, k.getAbsolutePath());
+		values.put(MediaStore.MediaColumns.TITLE, name);
+		values.put(MediaStore.MediaColumns.SIZE, k.length());
+		putDefaultValues();
+	}
+	
+	private void putDefaultValues(){
+		values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/mp3");
+		values.put(MediaStore.Audio.Media.ARTIST, "Soundboard");
+		values.put(MediaStore.Audio.Media.IS_RINGTONE, true);
+		values.put(MediaStore.Audio.Media.IS_NOTIFICATION, true);
+		values.put(MediaStore.Audio.Media.IS_ALARM, true);
+		values.put(MediaStore.Audio.Media.IS_MUSIC, false);	
 	}
 }
