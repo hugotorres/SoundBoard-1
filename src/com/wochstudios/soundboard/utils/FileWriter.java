eeee3f4e3e5da05c271172cf99a6c5359a6e0be0
Dave McPherson <dmcp89@gmail.com>
2015-05-06 11:45:19 -0400

    Update FileWriter.java
    Refactored writeFile method into 3 separate methods

--- src/com/wochstudios/soundboard/utils/FileWriter.java
+++ src/com/wochstudios/soundboard/utils/FileWriter.java
@@ -12,20 +12,26 @@ public class FileWriter {
 	public File writeFile(InputStream stream,String name){
 		 	
 		 try {
-			   InputStream ins = stream;
-			   byte[] buffer = new byte[ins.available()];
-			   ins.read(buffer);
-			   ins.close();
-			   String filename = Environment.getExternalStorageDirectory().toString()+File.separator+"download"+File.separator+name+".mp3";
-			   FileOutputStream fos = new FileOutputStream(filename);
-			   fos.write(buffer);
-			   fos.close();
-			      
+			String filename = Environment.getExternalStorageDirectory().toString()+File.separator+"download"+File.separator+name+".mp3";
+			copyInputStreamToOutputStream(stream, filename);   
 		   } catch (IOException io) {
 			   return null;
 		   }
 		   return new File(Environment.getExternalStorageDirectory().toString()+File.separator+"download"+File.separator+name+".mp3");
 		    
+	}
+	
+	private void copyInputStreamToOutputStream(InputStream stream, String file){
+		FileOutputStream fos = new FileOutputStream(file);
+		fos.write(getBuffeFromInputStream(stream));
+		fos.close();
+	}
+	
+	private byte[] getBuffeFromInputStream(InputStream stream){
+		byte[] buffer = new byte[ins.available()];
+		ins.read(buffer);
+		ins.close();
+		return buffer
 	}
 	
 }
