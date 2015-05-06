b40601a8f8346dc53af254b245bc84b122acbe4e
Dave McPherson <dmcp89@gmail.com>
2015-05-06 12:02:26 -0400

    Update MapLoader.java
    refactored loadValues method into 2 methods,

--- src/com/wochstudios/soundboard/utils/MapLoader.java
+++ src/com/wochstudios/soundboard/utils/MapLoader.java
@@ -7,20 +7,25 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class MapLoader {
-	
+	private final int KEY = 0;
+        private final int VALUE = 1;
+        private HashMap <String,String> valueMap = new HashMap<String,String>();
+        
 	public HashMap<String,String> loadVaules(InputStream valueFile) throws IOException{
-		final int lhs = 0;
-        final int rhs = 1;
-        HashMap<String,String> valueMap = new HashMap<String,String>();
-        BufferedReader  bfr = new BufferedReader(new InputStreamReader(valueFile));
-        String line;
-        while ((line = bfr.readLine()) != null) {
-            if (!line.equals("")){
-                String[] pair = line.trim().split(":");
-                valueMap.put(pair[lhs].trim(), pair[rhs].trim());
-            }
-        }
-        bfr.close();
-        return valueMap;
+        	BufferedReader  bfr = new BufferedReader(new InputStreamReader(valueFile));
+        	readBufferIntoMap(bfr);
+        	bfr.close();
+        	return valueMap;
+	}
+	
+	private void readBufferIntoMap(BufferedReader bfr){
+		String line = "";
+        	while ((line = bfr.readLine()) != null) {
+            		if (!line.equals("")){
+                		String[] pair = line.trim().split(":");
+                		valueMap.put(pair[KEY].trim(), pair[VALUE].trim());
+            		}
+        	}
+        	return valeuMap;
 	}
 }
